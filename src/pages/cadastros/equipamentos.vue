c48fd6aba9968ea044a53c530e69221ccc194ed8
Galaraz <jvictor.si.2013@gmail.com>
2021-07-14 10:34:14 -0300

    Adiçao paginas de cadastro

---
+++ src/pages/cadastros/equipamentos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cadastro-page">
+    <h1 class="page-title">cadastro de equipamentos</h1>
+    
+    
+  </div>
+</template>
+
+<script>
+//import Widget from '@/components/Widget/Widget';
+//import BigStat from './components/BigStat/BigStat';
+//import mock from './mock';
+
+//import { Chart } from 'highcharts-vue';
+
+export default {
+  name: 'equipamentos',
+  components: {
+    //Widget, BigStat, highcharts: Chart
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+
---
+++ src/pages/cadastros/usuarios.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cadastros-page">
+    <h1 class="page-title">Cadastro de Usuarios</h1>
+    
+    
+  </div>
+</template>
+
+<script>
+//import Widget from '@/components/Widget/Widget';
+//import BigStat from './components/BigStat/BigStat';
+//import mock from './mock';
+
+//import { Chart } from 'highcharts-vue';
+
+export default {
+  name: 'Usuarios',
+  components: {
+    //Widget, BigStat, highcharts: Chart
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+
